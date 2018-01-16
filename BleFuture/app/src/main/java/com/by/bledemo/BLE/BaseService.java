875c9e19f7e04665cc2befcf209b789b45184560
lin82092001 <lin82092001@gmail.com>
2018-01-16 23:19:59 +0800

    2018/1/16

--- BleFuture/app/src/main/java/com/by/bledemo/BLE/BaseService.java
+++ BleFuture/app/src/main/java/com/by/bledemo/BLE/BaseService.java
@@ -11,10 +11,14 @@ import java.util.UUID;
 /**
  * Created by 林北94狂 on 2018/1/10.
  */
-
+    /*
+      Gatt Service contains a collection of BluetoothGattCharacteristic, as well as referenced services.
+    * A GATT characteristic is a basic data element used to construct a GATT service, BluetoothGattService.
+    * The characteristic contains a value as well as additional information and optional GATT descriptors, BluetoothGattDescriptor.
+    */
 public class BaseService {
     private String Name;
-    public BluetoothGattService Service;    //Represents a Bluetooth GATT Service
+    public BluetoothGattService GattService;    //Represents a Bluetooth GATT Service
     public UUID Service_UUID;   //An immutable universally unique identifier (UUID). A UUID represents a 128-bit value.
     public List<BluetoothGattCharacteristic> Characteristics;   //Represents a Bluetooth GATT Characteristic
     public List<UUID> CharacteristicUUID;
@@ -34,16 +38,16 @@ public class BaseService {
     {
         return Name;
     }
-    public void InitService()
+    public void InitService()   //Initial GATT services
     {
         this.Characteristics.clear();
-        if(this.Service != null)
+        if(this.GattService != null)    //if find GATT services,show out characteristic from UUID or list of characteristic
         {
             for(UUID CharID : this.CharacteristicUUID)
             {
                 if(CharID != null)
                 {
-                    BluetoothGattCharacteristic ch = this.Service.getCharacteristic(CharID);    //Returns a characteristic with a given UUID out of the list of characteristics offered by this service.
+                    BluetoothGattCharacteristic ch = this.GattService.getCharacteristic(CharID);    //Returns a characteristic with a given UUID out of the list of characteristics offered by this service.
                     if (ch == null)
                     {
                         if (this.listener != null)
@@ -56,7 +60,7 @@ public class BaseService {
                 }
                 else
                 {
-                    for(BluetoothGattCharacteristic ch:this.Service.getCharacteristics())
+                    for(BluetoothGattCharacteristic ch:this.GattService.getCharacteristics())   //Returns a list of characteristics included in this service.
                     {
                         Log.d(this.Name(),"Find UUID:"+ch.getUuid().toString());
                         this.Characteristics.add(ch);   //add all
--- BleFuture/app/src/main/java/com/by/bledemo/BLE/DeviceManager.java
+++ BleFuture/app/src/main/java/com/by/bledemo/BLE/DeviceManager.java
@@ -1,8 +1,90 @@
 package com.by.bledemo.BLE;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothGatt;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
+import android.content.Context;
+import android.os.Handler;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedDeque;
+
 /**
  * Created by 林北94狂 on 2018/1/10.
  */
 
 public class DeviceManager {
+
+    private class Data2Write
+    {
+        public boolean type;
+        public Object Dest;
+        public byte[] data;
+
+        public Data2Write(BluetoothGattCharacteristic chars,byte[] data)
+        {
+            type = false;
+            this.Dest = chars;
+            this.data = data;
+        }
+        /*
+        * GATT Descriptors contain additional information and attributes of a GATT characteristic, BluetoothGattCharacteristic.
+        * They can be used to describe the characteristic's features or to control certain behaviours of the characteristic.
+        */
+        public Data2Write(BluetoothGattDescriptor desc, byte[] data)
+        {
+            type = true;
+            this.Dest = desc;
+            this.data = data;
+        }
+    }
+    protected final UUID GATT_CLIENT_CHAR_CFG_UUID=UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
+    protected BluetoothGatt mGatt;
+    protected BluetoothAdapter mBluetoothAdapter;
+    protected BluetoothDevice Device;
+    private Queue<Data2Write> SendData;
+    private Queue<BluetoothGattCharacteristic> ReadData;
+    private List<BaseService> Services;
+    private Listener listener;
+    private Context parent;
+    private Handler handler;
+    private Runnable RSSIReader;
+    private  final int DEFAULT_RSSI_PERI = 0;//Disable
+    private int RSSI;
+    private long RSSI_READ_PERI;
+    private boolean RSSI_Enable = false;
+    private boolean RSSI_Waitting = false;
+    private boolean Writing;
+    private boolean Reading;
+    private boolean Connected;
+
+    public DeviceManager()
+    {
+        this.Writing = false;
+        this.Reading = false;
+        this.Connected = false;
+        this.mGatt = null;
+        this.mBluetoothAdapter = null;
+        this.Device = null;
+        this.RSSI_READ_PERI = DEFAULT_RSSI_PERI;    //default 10ms
+        this.SendData=new ConcurrentLinkedDeque<>();    //Constructs an empty deque.
+        this.ReadData=new ConcurrentLinkedDeque<>();
+        /*
+        * If multiple threads access a linked list concurrently,
+        * and at least one of the threads modifies the list structurally, it must be synchronized externally.
+        * This is best done at creation time, to prevent accidental unsynchronized access to the list.
+        * */
+        this.Services= Collections.synchronizedList(new LinkedList<BaseService>()); //Returns a synchronized (thread-safe) list backed by the specified list.
+        this.handler = new Handler();
+    }
+    public boolean Connected()  //The device is connected.
+    {
+        return this.Device!=null && this.Connected && this.mGatt!=null;
+    }
 }
